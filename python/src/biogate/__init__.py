fa13144a6f544fd5d2a4c5f3df2fe2ac944924dc
Samuel Bharti <sbharti@uab.edu>
2026-07-09 04:55:50 -0500

    feat: add Python package skeleton

---
+++ python/src/biogate/__init__.py
@@ -0,0 +1,7 @@
+"""biogate: validate biological identifiers and inputs.
+
+This is an early scaffold. The public API described in the repository README is
+not implemented yet.
+"""
+
+__version__ = "0.1.0.dev0"
